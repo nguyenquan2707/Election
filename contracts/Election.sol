e359d8c6d63588c787760ff3824eb9ee079c7f77
nhovay8 <nhovay8@gmail.com>
2019-10-08 22:22:42 +0700

    first commit

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.8;
+
+contract Election {
+    //Store Candidate
+    //string candidate;
+    // Read Candidate
+    string public candidate;
+    //Contructor
+    constructor() public {
+        candidate = "Candidate1";
+    }
+}
