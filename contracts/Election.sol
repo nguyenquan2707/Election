58d6980d782d6ca0c211537bddc3d542d46eb9b5
nhovay8 <nhovay8@gmail.com>
2019-10-09 21:41:29 +0700

    add candidate function

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,26 @@
 pragma solidity ^0.5.8;
 
 contract Election {
-    //Store Candidate
-    //string candidate;
-    // Read Candidate
-    string public candidate;
+    //Model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint count;
+    }
+    //Store candidate
+    //Fetch candidate
+    //Store candidate count
+    mapping(uint => Candidate) public candidates;
+    //count
+    uint public count;
     //Contructor
     constructor() public {
-        candidate = "Candidate1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+        addCandidate("Candidate 3");
+    }
+    function addCandidate (string memory _name) private {
+        count ++;
+        candidates[count] = Candidate(count, _name, 0);
     }
 }
