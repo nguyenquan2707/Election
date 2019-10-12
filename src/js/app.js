a2d0dad86cf2ef1ec3b5dc5a3633ad21eb361edf
nhovay8 <nhovay8@gmail.com>
2019-10-12 22:58:01 +0700

    fix don't show on Chrome

--- src/js/app.js
+++ src/js/app.js
@@ -13,10 +13,12 @@ App = {
     if (typeof web3 !== 'undefined') {
       // If a web3 instance is already provided by Meta Mask.
       App.web3Provider = web3.currentProvider;
+      ethereum.enable();
       web3 = new Web3(web3.currentProvider);
     } else {
       // Specify default instance if no web3 instance provided
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      ethereum.enable();
       web3 = new Web3(App.web3Provider);
     }
     return App.initContract();
